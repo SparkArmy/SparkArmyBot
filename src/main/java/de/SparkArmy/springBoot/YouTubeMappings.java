f941e88b439e21d5cbc28c890739983bdcbbec78
Jonas Wischnewski <69726930+JonWichi@users.noreply.github.com>
2023-09-23 08:51:54 +0200

    Update YouTubeMappings.java
    Fix #48 in v2.0

--- src/main/java/de/SparkArmy/springBoot/YouTubeMappings.java
+++ src/main/java/de/SparkArmy/springBoot/YouTubeMappings.java
@@ -51,6 +51,10 @@ public class YouTubeMappings {
         String userId = entry.getString("yt:channelId");
         String videoId = entry.getString("yt:videoId");
 
+        LocalDateTime published = LocalDateTime.parse(entry.getString("published"), DateTimeFormatter.ISO_DATE_TIME);
+        LocalDateTime updated = LocalDateTime.parse(feed.getString("updated"),DateTimeFormatter.ISO_DATE_TIME);
+        if (published.plusMinutes(5).isAfter(updated)) return;
+        
         if (sentVideos.containsKey(userId)) {
             if (sentVideos.get(userId).equals(videoId)) return;
         }

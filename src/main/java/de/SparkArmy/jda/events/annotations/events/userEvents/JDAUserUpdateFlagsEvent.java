b0e48f93722b9b04e020f15ede93ea3d89026afa
JonWi <69726930+jonwichi@users.noreply.github.com>
2023-12-03 18:44:03 +0100

    Add Annotation
    - Add missing annotations and annotation calls

---
+++ src/main/java/de/SparkArmy/jda/events/annotations/events/userEvents/JDAUserUpdateFlagsEvent.java
@@ -0,0 +1,8 @@
+package de.SparkArmy.jda.events.annotations.events.userEvents;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface JDAUserUpdateFlagsEvent {
+}

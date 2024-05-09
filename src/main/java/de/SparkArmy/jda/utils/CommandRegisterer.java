9e0f4841661c4b0a857221c1ec1ec78581c747ce
Jonas Wischnewski <69726930+jonwichi@users.noreply.github.com>
2024-05-09 12:09:04 +0200

    Fix Category show in the list for archival channels
    - Add Collection with Guild-ChannelTypes in CommandRegisterer
    
    - Add modifier in command-data from archiveSlashCommand

--- src/main/java/de/SparkArmy/jda/utils/CommandRegisterer.java
+++ src/main/java/de/SparkArmy/jda/utils/CommandRegisterer.java
@@ -70,12 +70,25 @@ public class CommandRegisterer {
         }};
     }
 
+    @Contract(" -> new")
+    final @NotNull Collection<ChannelType> guildChannelTypes() {
+        return new ArrayList<>() {{
+            add(ChannelType.FORUM);
+            add(ChannelType.MEDIA);
+            add(ChannelType.NEWS);
+            add(ChannelType.STAGE);
+            add(ChannelType.TEXT);
+            add(ChannelType.VOICE);
+        }};
+    }
+
     @JDACommandData
     final @NotNull CommandData archiveSlashCommand() {
         return Commands.slash("archive", "Move the Channel in archive-category")
                 .addOptions(
                         new OptionData(OptionType.CHANNEL, "channel", "The channel you want to archive")
                                 .setRequired(true)
+                                .setChannelTypes(guildChannelTypes())
                 )
                 .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR))
                 .setGuildOnly(true);

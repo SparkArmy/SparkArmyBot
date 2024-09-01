c56c719dc43624cc7ddd7204ec842d767e832425
Jonas Wischnewski <69726930+jonwichi@users.noreply.github.com>
2024-09-01 15:44:55 +0200

    Update Kotlin and add new Dependency - Update Kotlin to 2.0.20 - Add exposed-kotlin-datetime - Add aedile-core 1.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ import org.apache.commons.io.FileUtils
 plugins {
     java
     application
-    id("org.jetbrains.kotlin.jvm") version "2.0.20-RC"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20-RC"
+    id("org.jetbrains.kotlin.jvm") version "2.0.20"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
@@ -32,7 +32,7 @@ dependencies {
     implementation("org.postgresql:postgresql:42.7.3")
     implementation("com.zaxxer:HikariCP:5.1.0")
 
-    // Twitch and Youtube
+    // Twitch and YouTube
     implementation("com.github.twitch4j:twitch4j:1.21.0")
     implementation("com.github.philippheuer.events4j:events4j-handler-reactor:0.12.1")
     implementation("com.google.apis:google-api-services-youtube:v3-rev20240514-2.0.0")
@@ -56,18 +56,21 @@ dependencies {
 
     implementation("io.ktor:ktor-serialization-kotlinx-json:${ktorVersion}")
 
+    // Exposed
     val exposedVersion = "0.53.0"
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-json:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
 
     // Other Dependencies
     implementation("org.json:json:20240303")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains:annotations:24.1.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.0.20-RC")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.0.20")
+    implementation("com.sksamuel.aedile:aedile-core:1.3.1")
 }
 
 tasks.register("clearResources") {

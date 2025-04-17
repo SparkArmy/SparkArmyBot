c68e3c61658dab2c38e9b8e0babeba1c71dd7a26
Jonas Wischnewski <69726930+jonwichi@users.noreply.github.com>
2025-04-17 23:19:17 +0200

    Update Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
-    kotlin("plugin.serialization") version "2.0.20"
-    id("com.google.cloud.tools.jib") version "3.4.2"
-    id("com.google.devtools.ksp") version "2.0.20-1.0.25"
+    kotlin("jvm") version "2.2.0-Beta1"
+    kotlin("plugin.serialization") version "2.2.0-Beta1"
+    id("com.google.cloud.tools.jib") version "3.4.5"
+    id("com.google.devtools.ksp") version "2.1.20-2.0.0"
     id("org.bytedeco.gradle-javacpp-platform") version "1.5.10"
     application
 }
@@ -11,6 +11,7 @@ kotlin {
     jvmToolchain(21)
     compilerOptions {
         freeCompilerArgs.add("-Xcontext-receivers")
+//        freeCompilerArgs.add("-Xcontext-parameters")
     }
 }
 
@@ -31,16 +32,16 @@ dependencies {
 
     // Database, Exposed & Cache
     implementation("org.postgresql:postgresql:42.7.5")
-    implementation("com.zaxxer:HikariCP:5.1.0")
-    implementation("org.flywaydb:flyway-core:10.17.3")
-    implementation("org.flywaydb:flyway-database-postgresql:10.17.3")
-    implementation("com.sksamuel.aedile:aedile-core:1.3.1")
+    implementation("com.zaxxer:HikariCP:6.3.0")
+    implementation("org.flywaydb:flyway-core:11.7.1")
+    implementation("org.flywaydb:flyway-database-postgresql:11.7.1")
+    implementation("com.sksamuel.aedile:aedile-core:2.0.3")
 
-    implementation("io.insert-koin:koin-core:4.0.0-RC1")
-    implementation("io.insert-koin:koin-annotations:1.4.0-RC4")
-    ksp("io.insert-koin:koin-ksp-compiler:1.3.1")
+    implementation("io.insert-koin:koin-core:4.0.4")
+    implementation("io.insert-koin:koin-annotations:2.0.0")
+    ksp("io.insert-koin:koin-ksp-compiler:2.0.0")
 
-    val exposedVersion = "0.60.0"
+    val exposedVersion = "0.61.0"
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
@@ -54,21 +55,20 @@ dependencies {
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.0")
 
     // Ktor
-    val ktorVersion = "2.3.12"
+    val ktorVersion = "3.1.2"
     implementation("io.ktor:ktor-serialization-kotlinx-json:${ktorVersion}")
 
 
 
     // Other Dependencies
-    implementation("org.json:json:20240303")
+    implementation("org.json:json:20250107")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("org.jetbrains:annotations:24.1.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.0.20")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
-    implementation("dev.reformator.stacktracedecoroutinator:stacktrace-decoroutinator-jvm:2.4.4")
+    implementation("org.jetbrains:annotations:26.0.2")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.2.0-Beta1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("dev.reformator.stacktracedecoroutinator:stacktrace-decoroutinator-jvm:2.4.8")
     implementation("org.tomlj:tomlj:1.1.1")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.0")
-    implementation("dev.reformator.stacktracedecoroutinator:stacktrace-decoroutinator-jvm:2.4.4")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
 }
 
 ksp {
